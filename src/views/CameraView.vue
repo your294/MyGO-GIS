b46e3a3511e2381f3763f5955b41e5be75388539
chenjj <2548077705@qq.com>
2024-04-17 17:20:41 +0800

    finish camera

--- src/views/CameraView.vue
+++ src/views/CameraView.vue
@@ -6,24 +6,45 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-const videoElement = document.getElementById("cameraVideo");
 
-navigator.mediaDevices
-  .getUserMedia({ video: true })
-  .then(function (stream) {
-    if (videoElement === null) {
-      console.error("video element is null");
-    }
-    (videoElement as HTMLMediaElement).srcObject = stream;
-  })
-  .catch(function (error) {
-    console.error("获取摄像头失败:", error);
-  });
+let traceCameraStream: any = null;
+
+onMounted(() => {
+  const videoElement = document.getElementById("cameraVideo");
+
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      if (videoElement === null) {
+        console.error("video element is null");
+      }
+      (videoElement as HTMLMediaElement).srcObject = stream;
+      traceCameraStream = stream;
+    })
+    .catch(function (error) {
+      console.error("获取摄像头失败:", error);
+    });
+})
+
 
 const quitCamera = () => {
+  traceCameraStream.getTracks().forEach((track: any) => {
+    if (track.readyState == 'live') {
+          track.stop();
+      }
+  })
+  
+  const videoElement = document.getElementById("cameraVideo");
+  (videoElement as HTMLMediaElement).srcObject = null;
   router.back();
 };
+
+onUnmounted(() => {
+  // const videoElement = document.getElementById("cameraVideo");
+  // (videoElement as HTMLMediaElement).srcObject = null;
+})
 </script>
